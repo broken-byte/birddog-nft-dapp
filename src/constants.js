88895763f4015dfb126e3ade79bea33f7f243687
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-08-01 17:32:12 -0700

    Created and moved constants file

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,9 @@ const abiPathTestnet = "/config/abi-testnet.json";
 const abiPathMainnet = "/config/abi-mainnet.json";
 const isTestnet = true;
 
-export default Constants = {
+const Constants = {
   configFilePath: isTestnet ? configPathTestnet : configPathMainnet,
   abiFilePath: isTestnet ? abiPathTestnet : abiPathMainnet,
 };
+
+export default Constants;
